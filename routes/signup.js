15eacaea028f773d9f9a546239499404ca5251c7
betamee <gongxq95@gmail.com>
2017-02-15 20:00:40 +0800

    fix some bugs

--- routes/signup.js
+++ routes/signup.js
@@ -4,26 +4,25 @@ var sha1 = require('sha1');
 var express = require('express');
 var router = express.Router();
 
-
 var UserModel = require('../models/users');
 var checkNotLogin = require('../middlewares/check').checkNotLogin;
 
 // GET /signup 注册页
-router.get('/', checkNotLogin, function (req, res, next) {
+router.get('/', checkNotLogin, function(req, res, next) {
   res.render('signup');
 });
 
 // POST /signup 用户注册
-router.post('/', checkNotLogin, function (req, res, next) {
+router.post('/', checkNotLogin, function(req, res, next) {
   var name = req.fields.name;
   var gender = req.fields.gender;
   var bio = req.fields.bio;
-  var avatar = req.fields.avatar.path.split(path.sep).pop();
+  var avatar = req.files.avatar.path.split(path.sep).pop();
   var password = req.fields.password;
   var repassword = req.fields.repassword;
 
-  //校验参数
-   try {
+  // 校验参数
+  try {
     if (!(name.length >= 1 && name.length <= 10)) {
       throw new Error('名字请限制在 1-10 个字符');
     }
@@ -42,13 +41,14 @@ router.post('/', checkNotLogin, function (req, res, next) {
     if (password !== repassword) {
       throw new Error('两次输入密码不一致');
     }
-  } catch(e){
-    //注册失败，异步删除上传的头像
+  } catch (e) {
+    // 注册失败，异步删除上传的头像
     fs.unlink(req.files.avatar.path);
     req.flash('error', e.message);
     return res.redirect('/signup');
   }
-  //明文加密
+
+  // 明文密码加密
   password = sha1(password);
 
   // 待写入数据库的用户信息
@@ -59,14 +59,13 @@ router.post('/', checkNotLogin, function (req, res, next) {
     bio: bio,
     avatar: avatar
   };
-
+  // 用户信息写入数据库
   UserModel.create(user)
-    .then(function(result) {
-      //此user是插入mongodb后的值，包含 _id
-      user =result.ops[0];
+    .then(function (result) {
+      // 此 user 是插入 mongodb 后的值，包含 _id
+      user = result.ops[0];
       // 将用户信息存入 session
       delete user.password;
-      //这里设置req,意在当redirect后的req里有user字段，则能通过验证
       req.session.user = user;
       // 写入 flash
       req.flash('success', '注册成功');
@@ -74,11 +73,11 @@ router.post('/', checkNotLogin, function (req, res, next) {
       res.redirect('/posts');
     })
     .catch(function (e) {
-      //注册失败，异步删除上传的头像
+      // 注册失败，异步删除上传的头像
       fs.unlink(req.files.avatar.path);
-      //用户名被占用则跳回到注册页，而不是错误页
-      if(e.message.match('E11000 duplicate key')) {
-        req.flash('error', '用户名已经被占用');
+      // 用户名被占用则跳回注册页，而不是错误页
+      if (e.message.match('E11000 duplicate key')) {
+        req.flash('error', '用户名已被占用');
         return res.redirect('/signup');
       }
       next(e);
